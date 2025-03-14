786fe21b26e6dab7dd4bfd4d46e09fdac60a0bc4
dlqjawls <qjawls2617@naver.com>
2025-03-14 13:52:17 +0900

    ISSUE-174: 카카오 SDK config,service 분리

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -1,7 +1,24 @@
+buildscript {
+    val kotlinVersion = "1.9.0"
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+    
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
     }
 }
 
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'config/routes.dart';
 import 'providers/app_provider.dart';
 import 'screens/auth/login_screen.dart';
+import 'services/kakao_service.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-  );
-  KakaoSdk.init(nativeAppKey: 'eed918aa9f0ef754a93dec5247e9f38e');
+void main() async {
+  await AppInitializer.initialize();
   runApp(const MyApp());
 }
 
@@ -23,7 +18,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => AppProvider(),
       child: MaterialApp(
-        title: '네잎클로버',
+        title: '네입클로버',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
