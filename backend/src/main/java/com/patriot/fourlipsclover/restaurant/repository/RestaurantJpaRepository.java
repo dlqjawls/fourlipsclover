d6452dcd3ebd53703a24419dc292b5374bc98953
seonghyun-kim <aruesin2@gmail.com>
2025-03-18 16:30:42 +0900

    feature(restaurant): 리뷰 좋아요 기능
    1. 좋아요/싫어요 엔터티(복합키), 레포지토리 생성
    
    issue: #121

--- backend/src/main/java/com/patriot/fourlipsclover/restaurant/repository/RestaurantJpaRepository.java
+++ backend/src/main/java/com/patriot/fourlipsclover/restaurant/repository/RestaurantJpaRepository.java
@@ -1,5 +1,6 @@
 package com.patriot.fourlipsclover.restaurant.repository;
 
+import com.patriot.fourlipsclover.restaurant.dto.request.ReviewLikeCreate;
 import com.patriot.fourlipsclover.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface RestaurantJpaRepository extends JpaRepository<Restaurant, Long> {
 
 	Restaurant findByKakaoPlaceId(String kakaoPlaceId);
+	
+	void createLike(ReviewLikeCreate request);
 }
