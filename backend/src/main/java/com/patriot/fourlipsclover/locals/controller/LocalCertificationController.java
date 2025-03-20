69d9ee6dc1aa3719c40800423c02ab827aa51fb7
seonghyunKim <aruesin2@gmail.com>
2025-03-21 01:08:45 +0900

    feature(restaurant): 현지인 인증기능 스웨거 적용 issue: #122

--- backend/src/main/java/com/patriot/fourlipsclover/locals/controller/LocalCertificationController.java
+++ backend/src/main/java/com/patriot/fourlipsclover/locals/controller/LocalCertificationController.java
@@ -3,6 +3,7 @@ package com.patriot.fourlipsclover.locals.controller;
 import com.patriot.fourlipsclover.locals.dto.request.LocalCertificationCreate;
 import com.patriot.fourlipsclover.locals.dto.response.LocalCertificationResponse;
 import com.patriot.fourlipsclover.locals.service.LocalCertificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ public class LocalCertificationController {
 	private final LocalCertificationService localCertificationService;
 
 	@PostMapping("/{memberId}")
+	@Operation(summary = "지역인증 생성", description = "회원의 위치 정보를 기반으로 지역인증을 생성합니다")
 	public ResponseEntity<LocalCertificationResponse> create(
 			@RequestBody LocalCertificationCreate request,
 			@PathVariable(name = "memberId") Integer memberId) {
