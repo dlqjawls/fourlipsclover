9ca1e7d3038674a5db509d688459ce97f98e01b9
seonghyunKim <aruesin2@gmail.com>
2025-03-23 16:33:53 +0900

    feature(restaurant): 리뷰 이미지 저장기능 1. 이미지 저장 Controller, Service, config 구현 issue: #205

--- backend/src/main/java/com/patriot/fourlipsclover/config/MinioReviewConfig.java
+++ backend/src/main/java/com/patriot/fourlipsclover/config/MinioReviewConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MinioConfig {
+public class MinioReviewConfig {
 
 	@Value("${minio.url}")
 	private String minioUrl;
@@ -16,9 +16,18 @@ public class MinioConfig {
 
 	@Value("${minio.secretKey}")
 	private String secretKey;
-	
+
+	@Value("${minio.bucketName}")
+	private String bucketName;
+
+	//버킷을 다 다르게 추가하려고하는데,, 흠,,
 	@Bean
 	public MinioClient minioClient() {
 		return MinioClient.builder().endpoint(minioUrl).credentials(accessKey, secretKey).build();
+	}
+
+	@Bean
+	public String getBucketName() {
+		return this.bucketName;
 	}
 }
