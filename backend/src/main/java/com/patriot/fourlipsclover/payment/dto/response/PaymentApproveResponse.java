10a9fc9d2f373b56af39e6ea343e9bf1557c25df
seonghyun-kim <aruesin2@gmail.com>
2025-03-26 10:14:55 +0900

    hotfix(payment): 카카오 페이 결제 1. ApproveResponse 작성 issue: #240

--- backend/src/main/java/com/patriot/fourlipsclover/payment/dto/response/PaymentApproveResponse.java
+++ backend/src/main/java/com/patriot/fourlipsclover/payment/dto/response/PaymentApproveResponse.java
@@ -1,8 +1,59 @@
 package com.patriot.fourlipsclover.payment.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentApproveResponse {
+
+	@JsonProperty("aid")
+	private String aid;
+
+	@JsonProperty("tid")
+	private String tid;
+
+	@JsonProperty("cid")
+	private String cid;
+
+	@JsonProperty("sid")
+	private String sid;
+
+	@JsonProperty("partner_order_id")
+	private String partnerOrderId;
+
+	@JsonProperty("partner_user_id")
+	private String partnerUserId;
+
+	@JsonProperty("payment_method_type")
+	private String paymentMethodType;
+
+	@JsonProperty("amount")
+	private Amount amount;
+
+	@JsonProperty("card_info")
+	private CardInfo cardInfo;
+
+	@JsonProperty("item_name")
+	private String itemName;
+
+	@JsonProperty("item_code")
+	private String itemCode;
+
+	@JsonProperty("quantity")
+	private Integer quantity;
+
+	@JsonProperty("created_at")
+	private LocalDateTime createdAt;
+
+	@JsonProperty("approved_at")
+	private LocalDateTime approvedAt;
+
+	@JsonProperty("payload")
+	private String payload;
 
 }
