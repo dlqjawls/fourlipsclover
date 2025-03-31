2aded5b648bf754cc12a9bb6b1d2168a5dea4668
SpecialSHipDobby <dkdi4224@naver.com>
2025-03-31 16:32:20 +0900

    feature(Match, Payment): 	1. 매칭 결제용 readyForMatch 메서드 추가 	2. 신청자 - 매칭 신청 목록 조회 : matchId 포함해서 리턴 	3. 현지인 - 매칭 신청 들어온 목록 조회 : requestForm 객체 전체 반환 추가
    issue: #229

--- backend/src/main/java/com/patriot/fourlipsclover/match/dto/response/LocalsMatchListResponse.java
+++ backend/src/main/java/com/patriot/fourlipsclover/match/dto/response/LocalsMatchListResponse.java
@@ -1,24 +1,25 @@
 package com.patriot.fourlipsclover.match.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.patriot.fourlipsclover.match.entity.GuideRequestForm;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class LocalsMatchListResponse {
 
     private Integer matchId;       // 매칭 ID
+    private GuideRequestForm guideRequestForm;
     private Long applicantId;      // 신청자(매칭 요청자)의 회원 ID
     private String regionName;     // 지역 이름
-    private LocalDate startDate;   // 시작일자
-    private LocalDate endDate;     // 종료일자
     private LocalDateTime createdAt;  // 매칭 생성 일시
     private String status;         // 매칭 상태 (PENDING, CONFIRMED, REJECTED, CANCELED)
     private Integer price;         // 팁 금액(상품 테이블 만들어서 가져올 예정)
--- backend/src/main/java/com/patriot/fourlipsclover/match/dto/response/MatchDetailResponse.java
+++ backend/src/main/java/com/patriot/fourlipsclover/match/dto/response/MatchDetailResponse.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class MatchDetailResponse {
 
+    private Integer matchId;
     private String regionName;
     private String guideNickname;
     private String status;  // 상태 (PENDING, CONFIRMED, REJECTED)
