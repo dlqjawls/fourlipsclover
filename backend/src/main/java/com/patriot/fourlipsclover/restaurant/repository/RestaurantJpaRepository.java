80dd3c2d180a2559ea0e1c33dc1b2b9fb54afb75
seonghyun-kim <aruesin2@gmail.com>
2025-04-02 13:55:57 +0900

    hotfix(restaurant): 식당 검색 기능 수정 1. 위도, 경도 입력 순서 올바르게 변경 issue: #305

--- backend/src/main/java/com/patriot/fourlipsclover/restaurant/repository/RestaurantJpaRepository.java
+++ backend/src/main/java/com/patriot/fourlipsclover/restaurant/repository/RestaurantJpaRepository.java
@@ -1,30 +1,29 @@
 package com.patriot.fourlipsclover.restaurant.repository;
 
 import com.patriot.fourlipsclover.restaurant.entity.Restaurant;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface RestaurantJpaRepository extends JpaRepository<Restaurant, Integer> {
 
-    Optional<Restaurant> findByKakaoPlaceId(String kakaoPlaceId);
+	Optional<Restaurant> findByKakaoPlaceId(String kakaoPlaceId);
 
-    @Query(value = "SELECT * FROM restaurant " +
-            "WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(x)) * " +
-            "cos(radians(y) - radians(:longitude)) + " +
-            "sin(radians(:latitude)) * sin(radians(x)))) <= :radius/1000",
-            nativeQuery = true)
-    List<Restaurant> findNearbyRestaurants(
-            @Param("latitude") Double latitude,
-            @Param("longitude") Double longitude,
-            @Param("radius") Integer radius);
+	@Query(value = "SELECT * FROM restaurant " +
+			"WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(y)) * " +
+			"cos(radians(x) - radians(:longitude)) + " +
+			"sin(radians(:latitude)) * sin(radians(y)))) <= :radius/1000",
+			nativeQuery = true)
+	List<Restaurant> findNearbyRestaurants(
+			@Param("latitude") Double latitude,
+			@Param("longitude") Double longitude,
+			@Param("radius") Integer radius);
 
-    Restaurant findByRestaurantId(Integer restaurantId);
-    
-    boolean existsByRestaurantId(Integer restaurantId);
+	Restaurant findByRestaurantId(Integer restaurantId);
+
+	boolean existsByRestaurantId(Integer restaurantId);
 }
