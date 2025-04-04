bb4f94c71ebf5697093c5e566167c7fd34945b51
SpecialSHipDobby <dkdi4224@naver.com>
2025-04-04 17:21:04 +0900

    hotfix(Tag): 	1. 매칭 생성 - 매칭별 태그 저장 로직 수정
    issue: #229

--- backend/src/main/java/com/patriot/fourlipsclover/match/dto/request/MatchCreateRequest.java
+++ backend/src/main/java/com/patriot/fourlipsclover/match/dto/request/MatchCreateRequest.java
@@ -3,7 +3,6 @@ package com.patriot.fourlipsclover.match.dto.request;
 import com.patriot.fourlipsclover.match.entity.GuideRequestForm;
 import com.patriot.fourlipsclover.member.entity.Member;
 import com.patriot.fourlipsclover.restaurant.entity.Region;
-import com.patriot.fourlipsclover.tag.dto.request.TagRequest;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -17,7 +16,7 @@ public class MatchCreateRequest {
 
     @NotNull(message = "태그는 필수입니다")
     @Size(min = 1, max = 3, message = "태그는 1-3개 사이여야 합니다")
-    private List<TagRequest> tags;
+    private List<Long> tags;
 
     @NotNull(message = "지역 정보는 필수입니다")
     private Region region;
