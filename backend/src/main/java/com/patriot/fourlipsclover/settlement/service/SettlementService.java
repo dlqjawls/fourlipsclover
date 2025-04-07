4bf4179f8da662d9b86654680876eb497feeeace
seonghyun-kim <aruesin2@gmail.com>
2025-04-07 11:51:11 +0900

    feature(mypage): 현지인 인증 중복 요청 방지
    issue: #320

--- backend/src/main/java/com/patriot/fourlipsclover/settlement/service/SettlementService.java
+++ backend/src/main/java/com/patriot/fourlipsclover/settlement/service/SettlementService.java
@@ -2,10 +2,8 @@ package com.patriot.fourlipsclover.settlement.service;
 
 import com.patriot.fourlipsclover.exception.PlanNotFoundException;
 import com.patriot.fourlipsclover.member.entity.Member;
-import com.patriot.fourlipsclover.payment.entity.PaymentApproval;
 import com.patriot.fourlipsclover.payment.repository.PaymentApprovalRepository;
 import com.patriot.fourlipsclover.plan.entity.Plan;
-import com.patriot.fourlipsclover.plan.entity.PlanMember;
 import com.patriot.fourlipsclover.plan.repository.PlanMemberRepository;
 import com.patriot.fourlipsclover.plan.repository.PlanRepository;
 import com.patriot.fourlipsclover.settlement.dto.response.ExpenseResponse;
@@ -70,23 +68,6 @@ public class SettlementService {
 		LocalDateTime startAt = settlement.getPlan().getStartDate().atStartOfDay();
 		LocalDateTime endAt = settlement.getPlan().getEndDate().atTime(LocalTime.MAX);
 		settlementRepository.save(settlement);
-		// 정산 참여자 결제 내역마다 모두 추가
-		List<PaymentApproval> paymentApprovals = paymentApprovalRepository.findByApprovedAtBetweenAndPartnerUserIdLike(
-				startAt, endAt,
-				String.valueOf(settlement.getTreasurer().getMemberId()));
-		List<PlanMember> planMembers = planMemberRepository.findByPlan_PlanId(planId);
-		for (PaymentApproval paymentApproval : paymentApprovals) {
-			Expense expense = new Expense();
-			expense.setPaymentApproval(paymentApproval);
-			expense.setSettlement(settlement);
-			expenseRepository.save(expense);
-			for (PlanMember planMember : planMembers) {
-				ExpenseParticipant expenseParticipant = new ExpenseParticipant();
-				expenseParticipant.setExpense(expense);
-				expenseParticipant.setMember(planMember.getMember());
-				expenseParticipantRepository.save(expenseParticipant);
-			}
-		}
 	}
 
 	@Transactional(readOnly = true)
